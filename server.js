9baa1da989051cfdd3b77f93ed3713d90554297f
Robert <robert.nogowski@gmail.com>
2023-04-26 10:08:51 +0200

    Another test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 4000
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello TDO Team (dev version 3) !!!\n')
+  response.end('Hello TDO Team (dev version test) !!!\n')
 })
 
 server.listen(port)
