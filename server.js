712759529c8e29229f3fac52ea3dce371c79420a
Robert <robert.nogowski@gmail.com>
2023-04-27 10:51:08 +0200

    edu time

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 4000
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello TDO Team (3 pods) !!!\n')
+  response.end('Hello TDO Team (3 pods) development !!!\n')
 })
 
 server.listen(port)
